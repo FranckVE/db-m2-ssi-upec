69322e48aa5ff72799e71a39e46538c104031340
Adrien Bertrand <adrien.ber@free.fr>
2013-04-18 15:20:59 +0200

    ajout classe compte

--- WebServiceBDD/src/test/Compte.java
+++ WebServiceBDD/src/test/Compte.java
@@ -2,6 +2,38 @@ package test;
 
 public class Compte {
 
+	private String numCompte;
+	private String type;
+	private float valeur;
+	
+	public Compte(String n, String t, float v) {
+		numCompte = n;
+		type = t;
+		valeur = v;
+	}
+	
 
+	public String getNumCompte() {
+		return numCompte;
+	}
 
+	public void setNumCompte(String numCompte) {
+		this.numCompte = numCompte;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public float getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(float valeur) {
+		this.valeur = valeur;
+	}
 }
