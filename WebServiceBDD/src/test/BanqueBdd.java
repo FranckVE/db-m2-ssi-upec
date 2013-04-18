4267e43c6510a9ceb9bf9d200c61393979ea39b0
Adrien Bertrand <adrien.ber@free.fr>
2013-04-18 15:35:32 +0200

    changement classe chargement bdd

--- WebServiceBDD/src/test/BanqueBdd.java
+++ WebServiceBDD/src/test/BanqueBdd.java
@@ -20,32 +20,29 @@ public class BanqueBdd {
 		System.out.println(gson);
 	}
 
-	public String getAccounts(String log) {
-		// (we don't really want to use ServletContext, just show that we
-		// could:)
-		String cle = "";
-		String num = "";
-		String type = "";
-		float balance = 0;
+	/**
+	 * Fonction récupérant dans la BdD tous les comptes d'une personne
+	 * log Login de la personne concernée
+	 */
+	public ArrayList<Compte> getAccounts(String log) {
+		ArrayList<Compte> listComptes = new ArrayList<Compte>();
+		
 		String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
 		String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
 		String url = String.format("jdbc:mysql://%s:%s/wsbanque", host, port);
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection Connexion = DriverManager.getConnection(url,
-					"admin3JrZAdc", "dUrDdRkl6DAV");
+			Connection Connexion = DriverManager.getConnection(url,"admin3JrZAdc", "dUrDdRkl6DAV");
 			Statement State = Connexion.createStatement();
 			String query = "SELECT * FROM comptes WHERE login ='" + log + "'";
 			try {
 				ResultSet resultat = State.executeQuery(query);
 
 				boolean passe = false;
-				// we wait for one answer
 				while (resultat.next()) {
-					num = resultat.getString("num_compte");
-					type = resultat.getString("type_compte");
-					balance = resultat.getFloat("argent");
+					listComptes.add(new Compte(resultat.getString("num_compte"), 
+							resultat.getString("type_compte"), resultat.getFloat("argent")));
 					passe = true;
 				}
 				if (!passe)
@@ -55,13 +52,10 @@ public class BanqueBdd {
 			}
 
 		} catch (SQLException | ClassNotFoundException e1) {
-			// pw.println("erreur de Requete"+
 			return e1.getLocalizedMessage();
 		}
 
-		return "login =" + log + "   cle = " + cle;
-
-		// return "Hello Jersey in plain text";
+		return listComptes;
 	}
 
 }
