31f5bca9957dd2d579fd5a57823961af9e915c11
Adrien Bertrand <adrien.ber@free.fr>
2013-04-18 16:11:57 +0200

    main + engine

--- WebServiceBDD/src/Gestionnaire/Engine.java
+++ WebServiceBDD/src/Gestionnaire/Engine.java
@@ -25,22 +25,22 @@ import sun.misc.BASE64Decoder;
  */
 public class Engine {
 
-	public String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
-	public String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
-	public String url = String.format("jdbc:mysql://%s:%s/wsbdd", host, port);
-	public Statement State = null;
+	private Statement state = null;
 
 	public Engine() {
+		String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+		String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+		String url = String.format("jdbc:mysql://%s:%s/wsbdd", host, port);
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection Connexion = DriverManager.getConnection(url,
-					"adminRf3g7If", "RPGjUx1GUsiX");
-			State = Connexion.createStatement();
+			Connection Connexion = DriverManager.getConnection(url,"adminRf3g7If","RPGjUx1GUsiX");
+			state = Connexion.createStatement();
 		} catch (SQLException | ClassNotFoundException e1) {
 		}
 	}
 
-	public RSAPublicKey verif_banque(String nom_banque, String hash) {
+	public RSAPublicKey verifBanque(String nom_banque, String hash) {
 		RSAPublicKey cle_pub = null;
 		String id_banque = null;
 		boolean verify = false;
@@ -49,13 +49,13 @@ public class Engine {
 
 		String query = "SELECT * FROM banques WHERE nom ='" + nom_banque + "'";
 		try {
-			ResultSet resultat = State.executeQuery(query);
+			ResultSet resultat = state.executeQuery(query);
 
 			boolean passe = false;
 			// we wait for one answer
 			while (resultat.next()) {
 				BASE64Decoder decoder = new BASE64Decoder();
-				modulus =decoder.decodeBuffer(resultat.getString("pubModulus"));
+				modulus = decoder.decodeBuffer(resultat.getString("pubModulus"));
 				exponent = decoder.decodeBuffer(resultat.getString("pubExponent"));
 				id_banque = resultat.getString("id");
 				passe = true;
@@ -65,36 +65,61 @@ public class Engine {
 		} catch (Exception e) {
 			return null;
 		}
-		
+
 		// faire verification du hash avec id_banque
 		cle_pub = CryptoUtils.getRSAPubKey(exponent, modulus);
 		verify = CryptoUtils.verify(id_banque, hash, cle_pub);
+
+		if (verify) return cle_pub;
+		return null;
+	}
+	
+	public void verifUser(String login, String mdp, String hash) {
+		String result = "OK";
+		boolean verify = false;
+		byte[] pubKey = null;
+
+		// Premier test de login / mdp
+		String query = "SELECT * FROM credentials WHERE login ='" + login + "' and mdp ='" + mdp +"'";
 		
-		if (verify) {
-			return cle_pub;
-		} else {
-			return null;
+		try {
+			ResultSet resultat = state.executeQuery(query);
+			boolean passe = false;
+
+			while (resultat.next()) {
+				BASE64Decoder decoder = new BASE64Decoder();
+				pubKey = decoder.decodeBuffer(resultat.getString("pubKey"));
+				passe = true;
+			}
+			if (!passe) return "REJECT";
+		} catch (Exception e) {
+			return "REJECT";
 		}
+
+		// TODO VERIFY du mdp et son hash avec la pubKey recup !!7
+		
+		//cle_pub = CryptoUtils.getRSAPubKey(exponent, modulus);
+		//verify = CryptoUtils.verify(id_banque, hash, cle_pub);
+
+		if (verify) return "OK";
+		return "REJECT";
 	}
 
 	// genere cle de session
-	public byte[] sessionKeyGenerator(){
+	public byte[] sessionKeyGenerator() {
 		
 		SecretKey sessionKey = CryptoUtils.initAES128();
 		return sessionKey.getEncoded();
-		
 	}
-	
-	public String packetGenerator(SecretKey sessionKey, RSAPublicKey pubKey){
-		
+
+	public String packetGenerator(SecretKey sessionKey, RSAPublicKey pubKey) {
+
 		String packet = null;
 		ByteBuffer bb = ByteBuffer.allocate(8);
 		byte[] time = new byte[8];
 		bb.putLong(System.currentTimeMillis()).get(time, 0, 8);
-		
-		
+
 		return packet;
 	}
-	
 
 }
--- WebServiceBDD/src/Gestionnaire/Main.java
+++ WebServiceBDD/src/Gestionnaire/Main.java
@@ -10,26 +10,31 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Main
  */
 public class Main extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	private Engine engine;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
+	@Path("/Main")
     public Main() {
         super();
-        // TODO Auto-generated constructor stub
+        Engine engine = new Engine();
     }
+	
+	@GET
+	@Produces( MediaType.TEXT_PLAIN )
+	public String sayPlainTextHello(@DefaultValue("error") @QueryParam("cipher") String cipher){
+		
+	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
